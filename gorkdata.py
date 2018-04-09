9c447b2f92df0d7fb2ae381c39db2912b64d7d19
Isak Lindhé <isak.e.lindhe@gmail.com>
2018-04-09 14:02:24 +0200

    string uuids because postgres is weird or something

--- gorkdata.py
+++ gorkdata.py
@@ -8,31 +8,24 @@ import psycopg2
 
 db = connect(environ.get('DATABASE_URL') or 'sqlite:////' + abspath('dev.db'))
 
-def gen_uuid():
-    return uuid4()
 
-def create_db(safe=True):
-    print('creating tables...')
-    db.create_tables([Node, User], safe=safe) # TODO dry this
-    print('tables created')
+def gen_id() -> str:
+    return str(uuid4())
 
-def populate_db():
-    try:
-        with db.atomic() as transaction:
-            Node.create(lat=55.723533, lon=13.214179, name="magic oval")
-            Node.create(lat=55.723367, lon=13.206387, name="mystical clearing")
-        print('db populated')
-    except pw.IntegrityError:
-        print('integrity error when populating db')
+
+def id_field():
+    return pw.CharField(max_length=40, primary_key=True, default=gen_id)
+
 
 # Models
+
 
 class BaseModel(pw.Model):
     class Meta:
         database = db
 
 class Node(BaseModel):
-    id = pw.UUIDField(default=uuid4)
+    id = id_field()
     name = pw.CharField(max_length=80)
     lat = pw.FloatField()
     lon = pw.FloatField()
@@ -40,6 +33,25 @@ class Node(BaseModel):
         return (lat, lon)
 
 class User(BaseModel, UserMixin):
-    id = pw.UUIDField(default=uuid4)
+    id = id_field()
     name = pw.CharField(max_length=30, unique=True)
     email = pw.CharField(max_length=254, unique=True) # standard max email length
+
+ALL_TABLES = [Node, User]
+
+# init stuff
+
+def create_db(purge=False):
+    if purge:
+        print('dropping tables')
+        db.drop_tables(ALL_TABLES)
+    print('creating tables...')
+    db.create_tables(ALL_TABLES) # TODO dry this
+    print('tables created')
+
+
+def populate_db():
+    with db.atomic() as transaction:
+        Node.create(lat=55.723533, lon=13.214179, name="magic oval")
+        Node.create(lat=55.723367, lon=13.206387, name="mystical clearing")
+    print('db populated')
